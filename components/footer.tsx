e04851cb20563ee1d793dc2dfff899277b46a42f
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-08-11 14:42:26 +0700

    feat: add Footer component for improved site layout and navigation

---
+++ components/footer.tsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900">
+      <div className="max-w-screen-xl mx-auto px-4 w-full py-10 md:py-16">
+        <div className="grid md:grid-cols-3 gap-7">
+          <div>
+            <Link href="/" className="mb-10 block">
+              <Image src="/logo.png" alt="Logo" width={128} height={49} />
+            </Link>
+            <p className="text-gray-400">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              pariatur natus beatae ipsum minima cum!
+            </p>
+          </div>
+          <div>
+            <div className="flex gap-20">
+              <div className="flex-1 md:flex-none">
+                <h4 className="mb-8 text-xl font-semibold text-white">Links</h4>
+                <ul className="list-item space-y-5 text-gray-400">
+                  <li>
+                    <Link href="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link href="/about">About Us</Link>
+                  </li>
+                  <li>
+                    <Link href="/room">Rooms</Link>
+                  </li>
+                  <li>
+                    <Link href="/contact">Contact Us</Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex-1 md:flex-none">
+                <h4 className="mb-8 text-xl font-semibold text-white">Legal</h4>
+                <ul className="list-item space-y-5 text-gray-400">
+                  <li>
+                    <Link href="#">Legal</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Term & Condition</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Payment Method</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Privacy Policy</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h4 className="mb-8 text-xl font-semibold text-white">
+              Newsletter
+            </h4>
+            <p className="text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipisicing.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-screen-xl mx-auto px-4 border-t border-gray-500 py-8 text-center text-base text-gray-500">
+        &copy; Copyright {new Date().getFullYear()} Bimss | All Right Reserved
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
