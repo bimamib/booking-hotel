dc0ed4270b15614b68cf7365b927aac60db9b857
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-08-25 13:29:14 +0700

    feat: wrap Google sign-in button in a form for server-side authentication

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -1,10 +1,18 @@
+import { signIn } from "@/auth";
 import { FaG } from "react-icons/fa6";
 
 export const LoginGoogleButton = () => {
   return (
-    <button className="flex items-center justify-center gap-2 w-full bg-blue-700 text-white font-medium py-3 px-6 text-base rounded-lg hover:bg-blue-600 cursor-pointer">
-      <FaG className="size-6" />
-      Sign In With Google
-    </button>
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <button className="flex items-center justify-center gap-2 w-full bg-blue-700 text-white font-medium py-3 px-6 text-base rounded-lg hover:bg-blue-600 cursor-pointer">
+        <FaG className="size-6" />
+        Sign In With Google
+      </button>
+    </form>
   );
 };
