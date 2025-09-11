c3c35ba9974e4bd4d782f8c1e3ca43618410adfa
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-09-11 17:15:31 +0700

    feat: wrap layout in SessionProvider to manage authentication state

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 import "./globals.css";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
@@ -14,17 +16,20 @@ export const metadata: Metadata = {
   description: "Website Booking Hotel Online",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <html lang="en">
       <body className={`${plusJakartaSans.variable} antialiased`}>
-        <Navbar />
-        <main className="bg-gray-50 min-h-screen ">{children}</main>
-        <Footer />
+        <SessionProvider session={session}>
+          <Navbar />
+          <main className="bg-gray-50 min-h-screen ">{children}</main>
+          <Footer />
+        </SessionProvider>
         {/* Add any additional scripts or components here if needed */}
       </body>
     </html>
