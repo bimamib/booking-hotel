e166f4dc1fa8b78369c775ea8a01344bb6393853
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-10-08 20:24:39 +0700

    feat: enhance CreateForm with image upload functionality and amenities checkbox

--- components/admin/room/create-form.tsx
+++ components/admin/room/create-form.tsx
@@ -1,3 +1,5 @@
+import { IoCloudUploadOutline } from "react-icons/io5";
+
 const CreateForm = () => {
   return (
     <form action="">
@@ -25,8 +27,37 @@ const CreateForm = () => {
               <span className="text-sm text-red-500 mt-2">message</span>
             </div>
           </div>
+          <div className="mb-4 grid md:grid-cols-3">
+            <input
+              type="checkbox"
+              name="amenities"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg"
+            />
+            <label className="ms-2 text-sm font-medium text-gray-900 capitalize">
+              Spa
+            </label>
+            <div aria-live="polite" aria-atomic="true">
+              <span className="text-sm text-red-500 mt-2">message</span>
+            </div>
+          </div>
         </div>
-        <div className="col-span-4 bg-white p-4"></div>
+        <div className="col-span-4 bg-white p-4">
+          <label
+            htmlFor="input-file"
+            className="flex flex-col mb-4 items-center justify-center aspect-video border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 relative"
+          >
+            <div className="flex flex-col items-center justify-center text-gray-500 pt-5 pb-6 z-10">
+              <div className="flex flex-col items-center justify-center">
+                <IoCloudUploadOutline className="size-8" />
+                <p className="mb-1 text-sm font-bold">Select Image</p>
+                <p className="text-xs">
+                  SVG, PNG, JPG, GIF, or Others (max: 4MB)
+                </p>
+              </div>
+            </div>
+            <input type="file" id="input-file" className="hidden" />
+          </label>
+        </div>
       </div>
     </form>
   );
