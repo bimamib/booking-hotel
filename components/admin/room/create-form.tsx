bd309e6c088fa7599646d07b927023051b798f3f
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-11-17 17:10:03 +0700

    feat: integrate saveRoom function with form validation and dynamic error messaging

--- components/admin/room/create-form.tsx
+++ components/admin/room/create-form.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { useRef, useState, useTransition } from "react";
+import { useActionState } from "react";
+import { saveRoom } from "@/lib/actions";
 import { type PutBlobResult } from "@vercel/blob";
 import { IoCloudUploadOutline, IoTrashOutline } from "react-icons/io5";
 import Image from "next/image";
 import { BarLoader } from "react-spinners";
 import { Amenities } from "@prisma/client";
+import clsx from "clsx";
 
 const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
   const inputFileRef = useRef<HTMLInputElement>(null);
@@ -49,8 +52,13 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
     });
   };
 
+  const [state, formAction, isPending] = useActionState(
+    saveRoom.bind(null, image),
+    null
+  );
+
   return (
-    <form action="">
+    <form action={formAction}>
       <div className="grid md:grid-cols-12 gap-5">
         <div className="col-span-8 bg-white p-4 shadow-sm rounded-xl">
           <div className="mb-4">
@@ -61,7 +69,9 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               placeholder="Room Name..."
             />
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.name}
+              </span>
             </div>
           </div>
           <div className="mb-4">
@@ -72,7 +82,9 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               placeholder="Description"
             ></textarea>
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.description}
+              </span>
             </div>
           </div>
           <div className="mb-4 grid md:grid-cols-3">
@@ -90,7 +102,9 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               </div>
             ))}
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.amenities}
+              </span>
             </div>
           </div>
         </div>
@@ -150,7 +164,9 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               placeholder="Capacity..."
             />
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.capacity}
+              </span>
             </div>
           </div>
           {/* Input untuk harga */}
@@ -162,14 +178,30 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               placeholder="Price..."
             />
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.price}
+              </span>
             </div>
           </div>
+          {/* General Message */}
+          {state?.message ? (
+            <div className="mb-4 bg-red-200 p-2">
+              <span className="text-sm text-gray-700 mt-2">
+                {state?.message}
+              </span>
+            </div>
+          ) : null}
           <button
             type="submit"
-            className="bg-orange-400 text-white w-full hover:bg-orange-500 py-2.5 px-6 md:px-10 text-lg font-semibold cursor-pointer rounded-lg"
+            className={clsx(
+              "bg-orange-400 text-white w-full hover:bg-orange-500 py-2.5 px-6 md:px-10 text-lg font-semibold cursor-pointer rounded-lg",
+              {
+                "opacity-50 cursor-progress": isPending,
+              }
+            )}
+            disabled={isPending}
           >
-            Save
+            {isPending ? "Saving..." : "Save"}
           </button>
         </div>
       </div>
