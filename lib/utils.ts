26021a272f7afa481d146f457f4f6c08f838a0e7
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-11-24 18:36:25 +0700

    feat: add formatCurrency utility function for currency formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,12 @@ export const formatDate = (dateStr: string) => {
   });
   return formatter.format(date);
 };
+
+export const formatCurrency = (amount: number) => {
+  const formatter = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    maximumSignificantDigits: 3,
+  });
+  return formatter.format(amount);
+};
