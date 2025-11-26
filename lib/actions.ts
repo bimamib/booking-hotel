1b248a83fa2eaa64c39baf8e22286ea0b2d36fcc
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-11-26 11:16:28 +0700

    feat: implement deleteRoom function to remove rooms and revalidate path

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,6 +3,8 @@
 import { prisma } from "@/lib/prisma";
 import { ContactSchema, RoomSchema } from "@/lib/zod";
 import { redirect } from "next/navigation";
+import { del } from "@vercel/blob";
+import { revalidatePath } from "next/cache";
 
 export const saveRoom = async (
   image: string,
@@ -76,5 +78,18 @@ export const ContactMessage = async (
     return { message: "Thanks for Contacting Us!" };
   } catch (error) {
     console.log(error);
+  }
+};
+
+// Delete Room
+export const deleteRoom = async (id: string, image: string) => {
+  try {
+    await del(image);
+    await prisma.room.delete({
+      where: { id },
+    });
+  } catch (error) {
+    console.log(error);
+    revalidatePath("/admin/room");
   }
 };
