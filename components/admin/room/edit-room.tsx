8bf74b9b809ec637859d6c7f925e800543296f3e
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-12-02 17:58:11 +0700

    feat: create EditRoom component for room editing functionality

---
+++ components/admin/room/edit-room.tsx
@@ -0,0 +1,16 @@
+import CreateForm from "@/components/admin/room/create-form";
+import { getAmenities } from "@/lib/data";
+
+const EditRoom = async ({ roomId }: { roomId: string }) => {
+  const amenities = await getAmenities();
+  if (!amenities) return null;
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Edit a Room</h1>
+      <CreateForm amenities={amenities} />
+    </div>
+  );
+};
+
+export default EditRoom;
