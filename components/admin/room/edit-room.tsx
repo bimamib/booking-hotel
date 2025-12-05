cb55b6bede72259b6fd4f8f2c8b0eb9fbc65ddc0
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-12-05 18:03:00 +0700

    feat: update EditRoom component to fetch room details and use EditForm for editing

--- components/admin/room/edit-room.tsx
+++ components/admin/room/edit-room.tsx
@@ -1,14 +1,18 @@
-import CreateForm from "@/components/admin/room/create-form";
-import { getAmenities } from "@/lib/data";
+import EditForm from "@/components/admin/room/edit-form";
+import { getAmenities, getRoomById } from "@/lib/data";
+import { notFound } from "next/navigation";
 
 const EditRoom = async ({ roomId }: { roomId: string }) => {
-  const amenities = await getAmenities();
-  if (!amenities) return null;
+  const [amenities, room] = await Promise.all([
+    getAmenities(),
+    getRoomById(roomId),
+  ]);
+  if (!amenities || !room) return notFound();
 
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-800 mb-4">Edit a Room</h1>
-      <CreateForm amenities={amenities} />
+      <EditForm amenities={amenities} room={room} />
     </div>
   );
 };
