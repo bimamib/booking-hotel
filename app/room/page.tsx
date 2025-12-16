6d539fed4b53178124a9c99fd4a8a996276f4008
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-12-16 14:33:19 +0700

    feat: update RoomPage to use RoomSkeleton for loading state representation

--- app/room/page.tsx
+++ app/room/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { Suspense } from "react";
 import HeaderSection from "@/components/header-section";
 import Main from "@/components/main";
+import RoomSkeleton from "@/components/skeletons/room-skeleton";
 
 export const metadata: Metadata = {
   title: "Rooms & Rates",
@@ -16,7 +17,7 @@ const RoomPage = () => {
         subTitle="Lorem ipsum dolor sit amet."
       />
       <div className="mt-10 px-4">
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<RoomSkeleton />}>
           <Main />
         </Suspense>
       </div>
