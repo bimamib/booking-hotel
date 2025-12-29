cc2475755744c3e1e837659ad9b93baa7c817a4e
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-12-29 07:12:05 +0700

    feat: enhance ReserveForm with name and phone number fields

--- components/reserve-form.tsx
+++ components/reserve-form.tsx
@@ -2,10 +2,11 @@
 import { useState } from "react";
 import { addDays } from "date-fns";
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const ReserveForm = () => {
   const StartDate = new Date();
-  const EndDate = addDays(StartDate, 1)
+  const EndDate = addDays(StartDate, 1);
 
   const [startDate, setStartDate] = useState(StartDate);
   const [endDate, setEndDate] = useState(EndDate);
@@ -14,8 +15,8 @@ const ReserveForm = () => {
     const [start, end] = dates;
     setStartDate(start);
     setEndDate(end);
-  }
-  
+  };
+
   return (
     <div>
       <form action="">
@@ -38,6 +39,40 @@ const ReserveForm = () => {
             <p className="text-sm text-red-500 mt-2">message</p>
           </div>
         </div>
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-medium text-gray-900">
+            Your Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            className="py-2 px-4 rounded-lg border border-gray-300 w-full"
+            placeholder="Full Name..."
+          />
+          <div aria-live="polite" aria-atomic="true">
+            <p className="text-sm text-red-500 mt-2">message</p>
+          </div>
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-medium text-gray-900">
+            Phone Number
+          </label>
+          <input
+            type="text"
+            name="phone"
+            className="py-2 px-4 rounded-lg border border-gray-300 w-full"
+            placeholder="Phone Number..."
+          />
+          <div aria-live="polite" aria-atomic="true">
+            <p className="text-sm text-red-500 mt-2">message</p>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="px-10 py-3 text-center font-semibold text-white w-full bg-orange-400 rounded-lg cursor-pointer hover:bg-orange-500"
+        >
+          Reserve
+        </button>
       </form>
     </div>
   );
