5635d77f4d9a7775c74c3e35ea56311aa085a44c
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2026-02-01 01:59:49 +0700

    feat: enhance ReserveForm to support disabled date intervals for date selection

--- components/reserve-form.tsx
+++ components/reserve-form.tsx
@@ -4,10 +4,16 @@ import { addDays } from "date-fns";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { createReserve } from "@/lib/actions";
-import { RoomDetailProps } from "@/types/room";
+import { RoomDetailProps, DisabledDateProps } from "@/types/room";
 import clsx from "clsx";
 
-const ReserveForm = ({ room }: { room: RoomDetailProps }) => {
+const ReserveForm = ({
+  room,
+  disabledDate,
+}: {
+  room: RoomDetailProps;
+  disabledDate: DisabledDateProps[];
+}) => {
   const StartDate = new Date();
   const EndDate = addDays(StartDate, 1);
 
@@ -22,8 +28,15 @@ const ReserveForm = ({ room }: { room: RoomDetailProps }) => {
 
   const [state, formAction, isPending] = useActionState(
     createReserve.bind(null, room.id, room.price, startDate, endDate),
-    null
+    null,
   );
+
+  const excludeDates = disabledDate.map((item) => {
+    return {
+      start: item.startDate,
+      end: item.endDate,
+    };
+  });
 
   return (
     <div>
@@ -39,6 +52,7 @@ const ReserveForm = ({ room }: { room: RoomDetailProps }) => {
             minDate={new Date()}
             selectsRange={true}
             onChange={handleDateChange}
+            excludeDateIntervals={excludeDates}
             dateFormat={"dd-MM-YYYY"}
             wrapperClassName="w-full"
             className="py-2 px-4 rounded-lg border border-gray-300 w-full"
@@ -81,7 +95,7 @@ const ReserveForm = ({ room }: { room: RoomDetailProps }) => {
             "px-10 py-3 text-center font-semibold text-white w-full bg-orange-400 rounded-lg cursor-pointer hover:bg-orange-500",
             {
               "opacity-50 cursor-progress": isPending,
-            }
+            },
           )}
           disabled={isPending}
         >
