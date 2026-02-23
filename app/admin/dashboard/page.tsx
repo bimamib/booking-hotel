e8e4add0e1d56688d14230f5c0bfe87a07e650fc
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2026-02-23 12:43:31 +0700

    feat: implement DashboardPage with loading state and DashboardCards component

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Metadata } from "next";
+import DashboardCards from "@/components/admin/dashboard-cards";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 const DashboardPage = () => {
-  return <div className="py-20 mt-10">Dashboard Page</div>;
+  return (
+    <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
+      <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
+      <Suspense fallback={<p>Loading Cards...</p>}>
+        <DashboardCards />
+      </Suspense>
+    </div>
+  );
 };
 
 export default DashboardPage;
