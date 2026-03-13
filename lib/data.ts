ca2e2c63f921045594a218f10a8c9e655a4af31f
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2026-03-13 10:31:41 +0700

    feat: Implement initial database schema including a Contact model, add data fetching utilities, and create the admin dashboard page.

--- lib/data.ts
+++ lib/data.ts
@@ -170,3 +170,40 @@ export const getTotalCustomers = async () => {
     console.log(error);
   }
 };
+
+export const getReservations = async () => {
+  const session = await auth();
+  if (
+    !session ||
+    !session.user ||
+    !session.user.id ||
+    session.user.role !== "admin"
+  ) {
+    throw new Error("Unauthorized Access");
+  }
+  try {
+    const result = await prisma.reservation.findMany({
+      include: {
+        Room: {
+          select: {
+            name: true,
+            image: true,
+            price: true,
+          },
+        },
+        User: {
+          select: {
+            name: true,
+            email: true,
+            phone: true,
+          },
+        },
+        Payment: true,
+      },
+      orderBy: { createdAt: "desc" },
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
