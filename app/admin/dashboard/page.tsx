7825b72ecb608411cec19dabe945d29fba3c721e
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2026-03-14 11:29:26 +0700

    feat: Introduce admin dashboard page and establish initial database schema with room, reservation, user, and contact models.

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import DashboardCards from "@/components/admin/dashboard-cards";
+import ReservationList from "@/components/admin/reservation-list";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
@@ -12,6 +13,9 @@ const DashboardPage = () => {
       <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
       <Suspense fallback={<p>Loading Cards...</p>}>
         <DashboardCards />
+      </Suspense>
+      <Suspense fallback={<p>Loading Reservation...</p>}>
+        <ReservationList />
       </Suspense>
     </div>
   );
