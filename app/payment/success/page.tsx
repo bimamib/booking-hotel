31037fa344a28621f193ebce0d4d38d5be53ac41
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2026-03-30 14:47:38 +0700

    feat: Enhance PaymentSuccess component with user feedback and navigation options

--- app/payment/success/page.tsx
+++ app/payment/success/page.tsx
@@ -16,5 +16,30 @@ const PaymentSuccess = async ({
   if (paymentStatus === "pending") redirect("/payment/pending");
   if (paymentStatus === "failure") redirect("/payment/failure");
 
-  return <div className="max-w-screen-xl px-4 mx-auto py-20 mt-12"></div>;
+  return (
+    <div className="max-w-screen-xl px-4 mx-auto py-20 mt-12">
+      <div className="p-6 md:mx-auto">
+        <HiCheckCircle className="text-green-600 w-20 h-20 mx-auto my-6" />
+        <div className="text-center">
+          <h3 className="md:text-2xl text-base text-gray-900 font-semibold text-center">
+            Payment Done!
+          </h3>
+          <p className="text-gray-600 my-2">
+            Thank You for completing your secure online Payment.
+          </p>
+          <p>Have a great day!</p>
+          <div className="py-10 text-center">
+            <Link
+              href="/myreservation"
+              className="px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3"
+            >
+              GO TO MY RESERVATION
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default PaymentSuccess;
